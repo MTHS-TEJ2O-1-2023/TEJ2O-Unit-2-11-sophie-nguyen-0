4b80301a8d11b1878a36fce47dca846568d0181d
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-24 12:46:25 -0400

    code

--- main.ts
+++ main.ts
@@ -5,4 +5,35 @@
  * This program generates 2 random numbers and shows them
 */
 
+basic.clearScreen()
+basic.showIcon(IconNames.Skull)
 
+// variables
+let firstNumber = randint(0, 99)
+let secondNumber = randint(0, 99)
+
+// show 1st number
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  basic.showString('1 : ' + firstNumber)
+})
+
+// show 2nd number
+input.onButtonPressed(Button.B, function () {
+  basic.clearScreen()
+  basic.showString('2 : ' + secondNumber)
+})
+
+// compare both numbers
+input.onGesture(Gesture.Shake, function () {
+  basic.clearScreen()
+
+  if (firstNumber < secondNumber) {
+    basic.showString(firstNumber + '<' + secondNumber)
+  } else {
+    basic.showString(firstNumber + '>' + secondNumber)
+  }
+
+  basic.pause(1000)
+  basic.showIcon(IconNames.Ghost)
+})
