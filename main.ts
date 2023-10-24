8cbba58e802ea999dd99a309aa57029984af053a
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-24 12:43:28 -0400

    readme

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sophie
+ * Created on: Oct 2023
+ * This program generates 2 random numbers and shows them
 */
 
-basic.showString('Hello, World!')
+
